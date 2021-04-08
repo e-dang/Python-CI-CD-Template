f15f456774e5d1206bc9dc84df4099d3f883a222
Eric Dang <edang830@gmail.com>
2021-04-08 14:16:16 -0700

    Add functional test

--- tests/functional/test_functional.py
+++ tests/functional/test_functional.py
@@ -1,5 +1,8 @@
 import pytest
 
+
 @pytest.mark.functional
-def test_functional():
-    assert True
+def test_home_page(driver, live_server):
+    driver.get(live_server.url + '/admin')
+    element = driver.find_element_by_tag_name('h1')
+    assert element.text == 'Django administration'
